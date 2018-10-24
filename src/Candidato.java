26f598602aeb5b6517cb76b59a1a1c62978cc648
randel <randelsouza88@gmail.com>
2018-10-23 23:48:21 -0300

    Update class Candidato description variable nome

--- src/Candidato.java
+++ src/Candidato.java
@@ -1,12 +1,12 @@
 
 // Classe Candidato apresenta os atributos e métodos necessários para que um canditado sejá armazenado na urna eletrônica
 public class Candidato {
-    private String nome;
+    private String nome; // Armazena o nome do Candidato(a)
     private int numero;
     private int votos;
     private String[] datas;
     private String funcao;
- 
+
     public Candidato(String nome, int numero, String funcao){
         this.nome = nome;
         this.numero = numero;
@@ -16,38 +16,38 @@ public class Candidato {
     }
     public String toString(){
         return "Nome: " + this.nome + " \n" + "Número: " + this.numero + " \n" + " Função: " + this.funcao;
-    } 
-    
+    }
+
     public String getFuncao() {
         return funcao;
     }
 
-    
+
     public void setFuncao(String funcao) {
         this.funcao = funcao;
     }
-    
+
     public String getNome() {
         return nome;
     }
-    
+
     public void setNome(String nome) {
         this.nome = nome;
     }
-    
+
     public int getNumero() {
         return numero;
     }
-   
+
     public void setNumero(int numero) {
         this.numero = numero;
     }
 
-   
+
     public int getVotos() {
         return votos;
     }
-   
+
     public void setVotos(int votos) {
         this.votos = votos;
     }
